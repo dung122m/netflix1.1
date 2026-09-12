e5f77557a4d52f818b0794f7724ad991b89aeac4
dungtran122 <dungtran122cq@gmail.com>
2026-09-13 00:11:45 +0700

    chore(lint): fix eslint config ignores for scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,12 +14,12 @@ const eslintConfig = defineConfig([
   },
   // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
     "scratch/**",
+    "scripts/**",
   ]),
 ]);
 
